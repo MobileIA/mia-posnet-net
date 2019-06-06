b955607201488a1f521f2a074feb42fb73981c72
Matias Camiletti <matias.camiletti@outlook.com>
2019-06-06 17:34:40 -0300

    Agregado prueba de venta con tarjeta.

--- Example/Form1.cs
+++ Example/Form1.cs
@@ -12,7 +12,7 @@ using System.Windows.Forms;
 
 namespace Example
 {
-    public partial class Form1 : Form, MobileiaPosnet.Services.CompleteService
+    public partial class Form1 : Form, MobileiaPosnet.Services.CompleteService, CompleteVentaService
     {
         public Form1()
         {
@@ -21,10 +21,18 @@ namespace Example
             MobileiaPosnet.Vx520.Config("COM3", "19200", "None", "One", "8");
         }
 
+        public void onCompleteSell(string code, string message, string client, string card)
+        {
+            label1.Text = "Resultado: " + code + " - " + message + " - " + client + " - " + card;
+        }
+
         private void btnTest_Click(object sender, EventArgs e)
         {
+            double amount = 30;
+
             MobileiaPosnet.Vx520 device = new MobileiaPosnet.Vx520();
-            device.ExecuteService(new TestService(listener: this));
+            device.ExecuteService(new VentaService(listener: this, amount: amount));
+            //device.ExecuteService(new TestService(listener: this));
         }
 
         void CompleteService.complete(bool success, string data)
